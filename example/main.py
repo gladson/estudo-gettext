0933c8289663a87156d903c1ddc278ee2a9dafc4
gladson <gladsonbrito@gmail.com>
2021-03-07 05:20:59 -0400

    refactor: add dir_name and list_langs
    * isSupportedLang and UnsupportedLanguage

--- example/main.py
+++ example/main.py
@@ -2,15 +2,30 @@ import gettext
 from pathlib import Path
 
 app_name = Path(__file__).absolute().joinpath().parts[-2]
-dirname = Path().absolute().joinpath("example/locale")
+dir_name = Path().absolute().joinpath("example/locale")
 
-t = gettext.translation(app_name, dirname)
-_ = t.gettext
-
-hello = _("Hello, ")
-
-world = _("world!")
+list_langs = gettext.find(app_name, dir_name, all=True, languages=["pt_BR", "en_US"])
 
 
-print(hello + world)
-print(gettext.find(app_name, dirname, all=True))
+def isSupportedLang(listTranslate, lang):
+    for item in listTranslate:
+        if lang == item.split("/")[-3]:
+            return lang
+    return None
+
+
+def UnsupportedLanguage(Exception):
+    pass
+
+
+if isSupportedLang(list_langs, "en_US"):
+    # t = gettext.translation(domain=app_name, localedir=dir_name, languages=['pt_BR', 'en_US'])
+    t = gettext.translation(domain=app_name, localedir=dir_name)
+    _ = t.gettext
+
+    hello = _("Hello, ")
+    world = _("world!")
+
+    print(hello + world)
+else:
+    raise UnsupportedLanguage("Language not found")
